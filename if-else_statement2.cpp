38ab3b5ff60af31e73e4da78154f78837a29203b
Doraemon-rk <arpitakumaripatwa@gmail.com>
2025-06-10 15:28:43 +0530

    basic if-else statement

---
+++ if-else_statement2.cpp
@@ -0,0 +1,185 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+
+
+    // Level 1: very basic understanding
+
+    int a ;
+    cout << "enter the value of a";
+    cin >> a ;
+
+    if (a > 0){
+        cout << " number is positive ";
+    }
+    else {
+        cout << "number is not positive";
+    }
+
+    int a ;
+    cout << "enter the value of a";
+    cin >> a;
+
+    if (a < 0){
+        cout << " number is  negative";
+
+    }
+    else {
+        cout << " number is not negative";
+    }
+
+
+    int a = 10;
+    cout << "enter the value of a";
+    cin >> a;
+
+    if (a > 10){
+        cout << " yes a greter than";
+    }
+    else {
+        cout << " no a greter than";
+    }
+    int a,b;
+    cout << "enter the value of a";
+    cin >> a;
+    cout << "enter the value of b";
+    cin >> b;
+    if (a == b){
+        cout << " yes a equal to b";
+    }
+    else {
+        cout << " no "; 
+    }
+
+
+    int x = 18;
+
+    cout << "enter the value of x";
+    cin >> x;
+
+    if (x >= 18 ){
+        cout << "eligible to vote";
+    }
+    else {
+        cout << " not eligible to vote "; 
+    }
+
+   // Level 2: slightly increased difficulty
+
+    int x,y;
+    cout << " enter the value of x";
+    cin >> x;
+
+    cout << " enter the value of y";
+    cin >> y;
+
+    if (x > y){
+        cout << " one is greater";
+    }
+    else {
+        cout << " one is not greater";
+    }
+
+
+    int z;
+    cout << " enter the value of z";
+    cin >> z;
+
+    if (z > 100){
+        cout << " it is greter than 100 ";
+    }
+    else {
+        cout << " it is not greter than 100";
+    }
+
+    int y;
+    cout << " enter the value of z";
+    cin >> y;
+
+    if (y < 50){
+        cout << " it is less than 50 ";
+    }
+    else {
+        cout << " it is not less than 50";
+    }
+
+
+    int b ;
+    cout << " enter the value of b";
+    cin >> b;
+
+    if (b = 13 and 19){ 
+        cout << " a person is a teenager";
+    }
+    else {
+        cout << " a person is not a teenager";
+    }
+
+   
+    int t = 40;
+    cout << " enter the value of t";
+    cin >> t;
+
+    if (t > 40 ){
+        cout << " temperature is too hot";
+    }
+    else {
+        cout << " temperature is hot";
+    }
+
+
+    //Level 3: Involving Ranges and multiple conditions
+
+    int marks = 33;
+    cout << " enter the value of marks";
+    cin >> marks;
+
+    if (marks >= 33){
+         cout << " the student has passed";
+    }
+    else {
+        cout << " the student has failed";
+    }
+
+
+     int r,s ;
+
+     cout << " enter the value of r";
+     cin >> r;
+
+     cout << " enter the value of s";
+     cin >> s ;
+
+     if ( r < s){
+        cout << " smaller";
+     }
+     else {
+        cout << " bigger";
+     }
+
+
+    int a ;
+    cout << " enter the value of a";
+    cin >> a;
+
+    if (a == 0){
+        cout << "a number is equal to 0";
+    }
+    else {
+        cout << " a number is not equal to 0";
+    }
+
+    int x = 13 and 19;
+    cout << "enter the value of x";
+    cin >> x ; 
+
+    if (x >= 13 && x <= 19){
+        cout << " a person is teenager";
+    }
+    else {
+        cout << " a person is not teenager";
+
+    
+    }
+}
