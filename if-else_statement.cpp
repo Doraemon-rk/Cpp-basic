ddea020b94266859cc9e8d90d8e66152955aa32e
Doraemon-rk <arpitakumaripatwa@gmail.com>
2025-06-10 14:26:39 +0530

    recommit

---
+++ if-else_statement.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+     int a;
+     cout << "enter the a " << endl;
+     cin >> a;
+
+     if (a % 2 == 0){
+    cout << "if it is even" << endl;
+ }
+     else {
+    cout << "if it is odd" << endl;
+ }
+}
