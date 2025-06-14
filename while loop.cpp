e0d94d7215f698f6637bb7ca499eaeb3505ba2a3
Doraemon-rk <arpitakumaripatwa@gmail.com>
2025-06-14 17:23:40 +0530

    Number from 0 to 100 using while loop

---
+++ while loop.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main (){
+
+    int num = 0;
+    while (num <= 100){
+        cout << num << " ";
+        num++;
+    }
+    cout << endl;
+    cout << "The last value of num is :- " << num << endl;
+    
+}
