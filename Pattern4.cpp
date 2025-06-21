b841f1f2dc081af205ae0dfee180d89e3d2df8a2
Doraemon-rk <arpitakumaripatwa@gmail.com>
2025-06-21 14:28:18 +0530

    Pattren using for loop

---
+++ Pattern4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main (){
+
+
+    int num = 16;
+
+    for (int i=1 ; i<=4 ; i++){
+
+        for (int j=1 ; j<=4 ; j++){
+        
+        cout << num << " ";
+        num++ ;    
+        }
+        cout << endl;
+    }
+}
