1b2eb1d0798bd6beaf390838ff11d79afe663285
Doraemon-rk <arpitakumaripatwa@gmail.com>
2025-06-30 21:29:26 +0530

    Hollow Diamond Pyramid

---
+++ Pattern20.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+int mian(){
+
+    
+       // Hollow Pyramid
+
+//       * 
+//      * * 
+//     *   * 
+//    *     * 
+//   *       * 
+//  *         * 
+//  *         * 
+//   *       * 
+//    *     * 
+//     *   * 
+//      * * 
+//       * 
+
+    for (int i=1 ; i<=6 ; i++){
+
+    for (int j=i ; j<=6 ; j++){
+      cout << " ";
+    }
+    for (int k=1 ; k<=i ; k++){
+      // cout << "* ";
+      if (k==1 || i==1 ||i==k  ){
+        cout << "* ";
+      }else {
+        cout << " "<< " ";
+      }
+    }
+    cout << endl;
+  }
+  for (int i=6 ; i>=1 ; i--){
+
+    for (int j=i ; j<=6 ; j++){
+      cout << " ";
+    }
+    for (int k=1 ; k<=i ; k++){
+      // cout << "* ";
+      if (k==1 || i==1 ||i==k  ){
+        cout << "* ";
+      }else {
+        cout << " "<< " ";
+      }
+    }
+    cout << endl;
+  }
+}
